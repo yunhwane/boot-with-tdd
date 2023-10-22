d4e56fd5a421f76b341365e3b411370028e68601
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2023-10-22 17:32:19 +0900

    메서드 분리 / http api 테스트 하는 방법 / log 확인법

---
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -0,0 +1,29 @@
+package com.example.productorderservice.product;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+class ProductService {
+
+    private final ProductPort productPort;
+
+    ProductService(final ProductPort productPort) {
+        this.productPort = productPort;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> addProduct(@RequestBody AddProductRequest request) {
+        final Product product = new Product(request.name(), request.price(), request.discountPolicy());
+
+        productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+}
