b6d652ea10ffe9c112c1af32f044d55f225001ff
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2023-10-23 21:21:23 +0900

    상품 주문 API 테스트

--- src/main/java/com/example/productorderservice/order/OrderService.java
+++ src/main/java/com/example/productorderservice/order/OrderService.java
@@ -1,10 +1,16 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
-
-@Component
+@RestController
+@RequestMapping("/orders")
 class OrderService {
     private final OrderPort orderPort;
 
@@ -12,12 +18,16 @@ class OrderService {
         this.orderPort = orderPort;
     }
 
-    public void createOrder(CreateOrderRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> createOrder(@RequestBody CreateOrderRequest request) {
         final Product product = orderPort.getProductById(request.productId());
 
         final Order order = new Order(product, request.quantity());
 
         orderPort.save(order);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
     }
 
 }
